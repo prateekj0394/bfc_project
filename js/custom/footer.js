5d0175799a5ddbe7ac675c8aece0034e56d3061f
SHIRSH ZIBBU <zibs.shirsh@gmail.com>
2014-08-29 01:39:18 +0530

    made compatible for new design
    removed previous garbage files - foundation5 css framework, old js, css,
    images

--- js/custom/footer.js
+++
@@ -1,6 +0,0 @@
-setInterval(function()
-{
-	var winHeight=$(window).height();
-	var footHeight=$("#footerid").height();
-	$("#footerid").css('top', winHeight-footHeight+"px");
-},1);
--- js/custom/submitBtn.js
+++
@@ -1,8 +0,0 @@
-submitBtn.onclick=function()
-{
-	var out="";
-	out += (fname.value?		fname.value: 	".") + "\n";
-	out += (uname.value?		uname.value: 	".") + "\n";
-	out += (domain.value?	domain.value: ".") + "\n";
-	alert(out);
-};
